7255cd4ccabf9a942261e1dc6dfd4e5d2583658a
glepnir <glephunter@gmail.com>
2023-10-27 17:09:58 +0800

    update

--- init.lua
+++ init.lua
@@ -1,1 +1,1 @@
-vim.opt.rtp:append("~/workspace/3danimation.nvim")
+vim.opt.rtp:append('~/workspace/3danimation.nvim')
--- plugin/3danimation.lua
+++ plugin/3danimation.lua
@@ -1,13 +1,13 @@
 if vim.g.loaded_3d then
-	return
+  return
 end
 
 vim.g.loaded_3d = true
 
-vim.api.nvim_create_user_command("Cube", function()
-	require("3danimation.cube")
+vim.api.nvim_create_user_command('Cube', function()
+  require('3danimation.cube')
 end, {})
 
-vim.api.nvim_create_user_command("Dounts", function()
-	require("3danimation.dounts")
+vim.api.nvim_create_user_command('Dounts', function()
+  require('3danimation.dounts')
 end, {})
